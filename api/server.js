75a74509e092488cb12b6be60499b594e700e36d
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-11 23:01:43 -0500

    finished for MVP

--- api/server.js
+++ api/server.js
@@ -27,4 +27,39 @@ server.get('/:id', (req, res) => {
 		});
 });
 
+server.post('/', (req, res) => {
+	db('accounts')
+		.insert(req.body)
+		.then((response) => {
+			res.status(201).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
+});
+
+server.delete('/:id', (req, res) => {
+	db('accounts')
+		.where('id', req.params.id)
+		.delete()
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
+});
+
+server.put('/:id', (req, res) => {
+	db('accounts')
+		.where('id', req.params.id)
+		.update(req.body)
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
+});
+
 module.exports = server;
