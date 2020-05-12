2e09d873132540552248e703ff8ebbb0aa684174
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-11 20:28:41 -0500

    finished the GET requests

--- api/server.js
+++ api/server.js
@@ -6,6 +6,25 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {});
+server.get('/', (req, res) => {
+	db('accounts')
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
+});
+
+server.get('/:id', (req, res) => {
+	db('accounts')
+		.where('id', req.params.id)
+		.then((response) => {
+			res.status(200).json(response);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
+});
 
 module.exports = server;
